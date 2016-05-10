8b53bbd63d57f3ef58488853bb17889ba2a8e63b
Damian Skirde <damianskirde@damians-macbook-air.local>
2016-05-10 20:40:36 +0200

    Initial Commit

---
+++ iOS_Course_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Course_App
+//
+//  Created by Damian Skirde on 2016/05/10.
+//  Copyright © 2016 Damian Skirde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
